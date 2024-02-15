de95cae43684791410599eb19c91f5092970ff45
BT_1N3_17 <sample@bths.edu>
2024-02-15 09:29:19 -0500

    fix bug that makes all the movies into avatar

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,3 +1,5 @@
+import com.sun.jdi.VMOutOfMemoryException;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -5,10 +7,62 @@ import java.util.Scanner;
 
 public class MovieCollection {
     private ArrayList<Movie> movieArrayList;
+    private Scanner scanner;
 
     public MovieCollection() {
         movieArrayList = new ArrayList<Movie>();
         loadMoviesInto();
+        sortMovies();
+        for (int i = 0; i < movieArrayList.size(); i++) {
+            System.out.println(movieArrayList.get(i).getName());
+        }
+        scanner = new Scanner(System.in);
+    }
+
+    public void menu() {
+
+        System.out.println("Welcome to the movie collection!");
+        String menuOption = "";
+
+        while (!menuOption.equals("q")) {
+            System.out.println("------------ Main Menu ----------");
+            System.out.println("- search (t)itles");
+            System.out.println("- search (c)ast");
+            System.out.println("- (q)uit");
+            System.out.print("Enter choice: ");
+            menuOption = scanner.nextLine();
+
+            if (menuOption.equals("t")) {
+                searchTitles();
+            } else if (menuOption.equals("c")) {
+                searchCast();
+            } else if (menuOption.equals("q")) {
+                System.out.println("Goodbye!");
+            } else {
+                System.out.println("Invalid choice!");
+            }
+        }
+    }
+
+    public void searchTitles() {
+
+    }
+
+    public void searchCast() {
+
+    }
+
+    public void sortMovies() {
+        for (int i = 1; i < movieArrayList.size(); i++) {
+            String name = movieArrayList.get(i).getName();
+            int innerIdx = i;
+            String otherName = movieArrayList.get(innerIdx - 1).getName();
+            while (innerIdx > 0 && name.compareTo(otherName) < 0) {
+                movieArrayList.set(innerIdx, movieArrayList.get(innerIdx - 1));
+                innerIdx--;
+            }
+            movieArrayList.set(innerIdx, movieArrayList.get(innerIdx));
+        }
     }
 
     private void loadMoviesInto() {
